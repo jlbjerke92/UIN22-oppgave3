e00739795bd5dc88fa1073f044ae0e79a46539cf
jlbjerke92 <78098481+jlbjerke92@users.noreply.github.com>
2022-01-26 19:08:40 +0100

    Add files via upload

---
+++ oppgave1.js
@@ -0,0 +1,25 @@
+import "./style.css";
+
+const text =
+  "Baby cliche unicorn brooklyn farm-to-table. Salvia semiotics hella literally paleo humblebrag bushwick letterpress messenger bag pork belly brooklyn authentic vexillologist. Gastropub sustainable banjo, shaman snackwave viral air plant ramps health goth. Edison bulb vegan microdosing, tote bag unicorn skateboard disrupt copper mug four loko sustainable whatever cloud bread slow-carb lumbersexual four dollar toast. Waistcoat lomo hammock vape shabby chic sartorial yr godard pok pok fashion axe organic migas. Quinoa yr vexillologist intelligentsia verylongwordthatislong neutra mixtape YOLO XOXO listicle letterpress farm-to-table beard.";
+
+const longestWord = () => {
+  
+    // Deler opp teksten 
+const textAsArray = text.split(" ");
+  
+// Setter første ord til å være det lengste så vi har noe å sammenlikne med
+let longestWord = textAsArray[0];
+  
+  
+  // Looper over alle ordene. Sjekker om ordet er lengre enn nåværende lengste ord. Hvis lengre oppdaterer vi lengste ord, hvis ikke brukes det ordet vi har
+    textAsArray.forEach((word) => {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  });
+  return longestWord;
+};
+
+//Skriver ut lengste ord i console.log
+console.log(longestWord());
